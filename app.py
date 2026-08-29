4dc278c5528c2a2022e05cd41a50b2266324b6a1
AribahMehboob <aribahdodesign@gmail.com>
2026-08-29 17:09:16 +0500

    Update app.py
    update port name

--- app.py
+++ app.py
@@ -559,5 +559,5 @@ async def serve_page(page: str):
 
 # ── Run Server ────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8001))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
